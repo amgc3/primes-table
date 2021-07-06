58c2dbebea5f7dd0d0059f771375612daf518eb5
Anna <adropofwater0@gmail.com>
2021-07-06 16:27:42 +0100

    isPrime returns true if n is 5 or 7

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const isPrime = (n) => {
   if (n <= 3) return true;
 
   if (n % 2 === 0 || n % 3 === 0) return false;
+
+  return true;
 };
 
 module.exports = {
--- index.spec.js
+++ index.spec.js
@@ -3,7 +3,7 @@ const { isPrime } = require('./index');
 describe('isPrime', () => {
   it('returns false if n is less than or equal to 1', () => {
     expect(isPrime(1)).toBe(false);
-    expect(isPrime(- 11)).toBe(false);
+    expect(isPrime(-11)).toBe(false);
   });
   it('returns true if n is equal to 2', () => {
     expect(isPrime(2)).toBe(true);
@@ -12,11 +12,18 @@ describe('isPrime', () => {
     expect(isPrime(3)).toBe(true);
   });
   it('returns false if n is a multiple of 2', () => {
-      expect(isPrime(8)).toBe(false);
-      expect(isPrime(22)).toBe(false);
+    expect(isPrime(8)).toBe(false);
+    expect(isPrime(22)).toBe(false);
   });
   it('returns false if n is a multiple of 3', () => {
     expect(isPrime(9)).toBe(false);
     expect(isPrime(27)).toBe(false);
-})
+  });
+  it('returns true if n is equal to 5', () => {
+    expect(isPrime(5)).toBe(true);
+  });
+  it('returns true if n ie equal to 7', () => {
+      expect(isPrime(7)).toBe(true);
+  });
+  
 });
